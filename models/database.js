4047db8784fbacd6284d45dc4cd55adf248931c1
plentini <pedrolentinicje@gmail.com>
2016-10-13 20:40:27 -0300

    token

--- models/database.js
+++ models/database.js
@@ -5,6 +5,6 @@ var connectionString = process.env.DATABASE_URL || 'postgres://carpooling:carpoo
 var client = new pg.Client(connectionString);
 client.connect();
 var query1 = client.query(
-	'CREATE TABLE users ( id serial primary key, username varchar(20), password varchar(20) ); CREATE TABLE clients ( name varchar(20), id varchar(20), secret varchar(20), userId varchar(20) );');
+	'CREATE TABLE users ( id serial primary key, username varchar(20) NOT NULL, password varchar(20) NOT NULL ); CREATE TABLE clients ( name varchar(20) NOT NULL, id char(20) NOT NULL, secret char(20) NOT NULL , userId char(20) NOT NULL ); CREATE TABLE codes ( value char(20) NOT NULL , redirectUri varchar(50) NOT NULL , userId char(20) NOT NULL , clientId char(20) NOT NULL ); CREATE TABLE tokens ( value char(20) NOT NULL , userId char(20) NOT NULL , clientId char(20) NOT NULL ); ');
 query1;
 console.log('Table users created.');
